a8cceb3b86bc6c7ec942da1248b836376274b2f4
Lokesh Cherukuri <lokesh.cherukuri@am.sony.com>
2018-12-09 13:51:52 -0800

    ES6 syntax

--- Q.js
+++ Q.js
@@ -2,21 +2,93 @@
 
     const domReadyCallbacks = [];
 
-    function Q(selector) {
-        this.nodes = [];
+    function _(selector) {
+        return new Q(selector);
+    }
+
+    class Q {
+        constructor(selector) {
+            this.nodes = [];
+            if (typeof selector === 'function') {
+                document.readyState === 'complete' ? selector.call(document) : domReadyCallbacks.push(selector);
+            } else if (typeof selector === 'string') {
+                this.nodes = document.querySelectorAll(selector);
+            } else if (selector instanceof HTMLElement) {
+                this.nodes = [selector];
+            } else if (selector instanceof NodeList) {
+                this.nodes = selector;
+            }
+        }
         
-        if (!(this instanceof Q)) {
-            return new Q(selector);
+        each(callback) {
+            const nodes = this.nodes;
+            for (var i=0; i<nodes.length; i++) {
+                callback.call(nodes[i]);
+            }
+            return this;
         }
 
-        if (typeof selector === 'function') {
-            document.readyState === 'complete' ? selector.call(document) : domReadyCallbacks.push(selector);
-        } else if (typeof selector === 'string') {
-            this.nodes = document.querySelectorAll(selector);
-        } else if (selector instanceof HTMLElement) {
-            this.nodes = [selector];
-        } else if (selector instanceof NodeList) {
-            this.nodes = selector;
+        addClass(classNames) {
+            const classes = classNames.split(' ');
+            return this.each(function() {
+                for (var i=0; i<classes.length; i++) {
+                    this.classList.add(classes[i]);
+                }
+            });
+        }
+
+        removeClass(classNames) {
+            const classes = classNames.split(' ');
+            return this.each(function() {
+                for (var i=0; i<classes.length; i++) {
+                    this.classList.remove(classes[i]);
+                }
+            });
+        }
+
+        text(text) {
+            if (text) {
+                this.each(function() {
+                    this.innerHTML = text;
+                });
+            }
+            return this.nodes.length && this.nodes[0].innerHTML;
+        }
+
+        on(event, callback) {
+            return this.each(function() {
+                this.addEventListener(event, function() {
+                    callback.call(document);
+                });
+            });
+        }
+
+        ajax(options) {
+            const xhr = {
+                successCallbacks: [],
+                failCallbacks: [],
+                done: function(callback) {
+                    this.successCallbacks.push(callback);
+                    return this;
+                },
+                fail: function(callback) {
+                    this.failCallbacks.push(callback);
+                    return this;
+                }
+            };
+    
+            const xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status === 200) {
+                    while (xhr.successCallbacks.length) {
+                        xhr.successCallbacks.pop().call(this, this.response);
+                    }
+                }
+            };
+            xmlHttp.open(options.method || "GET", options.URL, options.async || true);
+            xmlHttp.send();
+    
+            return xhr;
         }
     }
 
@@ -26,49 +98,6 @@
         }
     });
 
-    Q.prototype.each = function(callback) {
-        const nodes = this.nodes;
-        for (var i=0; i<nodes.length; i++) {
-            callback.call(nodes[i]);
-        }
-        return this;
-    }
-
-    Q.prototype.addClass = function(classNames) {
-        const classes = classNames.split(' ');
-        return this.each(function() {
-            for (var i=0; i<classes.length; i++) {
-                this.classList.add(classes[i]);
-            }
-        });
-    }
-
-    Q.prototype.removeClass = function(classNames) {
-        const classes = classNames.split(' ');
-        return this.each(function() {
-            for (var i=0; i<classes.length; i++) {
-                this.classList.remove(classes[i]);
-            }
-        });
-    }
-
-    Q.prototype.text = function(text) {
-        if (text) {
-            this.each(function() {
-                this.innerHTML = text;
-            });
-        }
-        return this.nodes.length && this.nodes[0].innerHTML;
-    }
-
-    Q.prototype.on = function(event, callback) {
-        return this.each(function() {
-            this.addEventListener(event, function() {
-                callback.call(document);
-            });
-        });
-    }
-
-    window.Q = Q;
+    window._ = _;
 
 })(window);
--- script.js
+++ script.js
@@ -1,34 +1,48 @@
 (function() {
 
+    // In jQuery, we use `$`. Here i a using `_`
+
     // Q.js should support onready callbacks
-    Q(function() {
+    _(function() {
         console.log('DOM is ready. Start manipulating it');
     });
 
     // Q.js should support looping through selected elements
-    Q('body').each(function() {
-        Q(this).addClass('italic');
+    _('body').each(function() {
+        _(this).addClass('italic');
     });
 
-    // Q.js should support querying with a selector and a class to the selected elements
-    Q('body').addClass('beige-background');
+    // Q.js should support querying with a selector and add a class to the selected elements
+    _('body').addClass('beige-background');
 
-    // Q.js should support multiple selectors and ability add/remove multiple classes
-    Q('p.p1,p.p2').addClass('color-red font-22');
+    // Q.js should support multiple selectors and ability to add/remove multiple classes
+    _('p.p1,p.p2').addClass('color-red font-22');
 
     // Q.js should support chaining
-    Q('p.p2').addClass('bold').removeClass('color-red');
+    _('p.p2').addClass('bold').removeClass('color-red');
 
     // Q.js should support get and set text content
-    Q('p.author').text('Lokesh Cherukuri');
+    _('p.author').text('Lokesh Cherukuri');
 
     // Q.js should support event listeners and chaining of events
-    Q('.button')
+    _('.button')
         .on('click', function() {
-            Q('.event-status').text('Button Clicked');
+            _('.event-status').text('Button Clicked');
         })
         .on('mouseover', function() {
-            Q('.event-status').text('Mouse hovered on button');
+            _('.event-status').text('Mouse hovered on button');
         });
+
+    // Q.js should support ajax calls
+    _().ajax({
+        method: 'GET',
+        URL: 'https://httpbin.org/get'
+    }).done(function(response) {
+        console.log("Ajax Done-1", response);
+    }).done(function(response) {
+        console.log("Ajax Done-2", response);
+    }).fail(function(error) {
+        console.log(error);
+    });
 
 })();
